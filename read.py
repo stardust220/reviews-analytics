b837e73c4e3290772f13d9b7d66a0072a1c6f3b5
stardust220 <r01941003@gmail.com>
2019-08-03 14:59:07 +0900

    calc average reviews

--- read.py
+++ read.py
@@ -3,4 +3,8 @@ with open('reviews.txt','r') as f:
     for line in f:
         data.append(line)
 
-print(len(data))
+sum_len = 0
+for d in data:
+    sum_len = sum_len + len(d)
+
+print("the average of review lens is", sum_len/len(d))
