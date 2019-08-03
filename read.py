5744117f454e36a315f2ede5169aad42a447ee6e
stardust220 <r01941003@gmail.com>
2019-08-03 17:17:44 +0900

    add filter function

--- read.py
+++ read.py
@@ -8,3 +8,11 @@ for d in data:
     sum_len = sum_len + len(d)
 
 print("the average of review lens is", sum_len/len(d))
+
+# filter the data if length is less than 100 words
+new = []
+for d in data:
+    if len(d) < 100:
+        new.append(d)
+
+print("the total number of the review which less than 100 words are", len(new))
